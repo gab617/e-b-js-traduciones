d6afbd56371675506017254d0ded25ffacbe98d6
gabo <gabi.280795@gmail.com>
2024-06-27 02:54:44 -0300

    nw endopints

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,39 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+/* const fs = require('fs');
+const path = require('path'); */
 
 
+function findElementById(data, id) {
+  for (const category in data) {
+    if (data.hasOwnProperty(category)) {
+      const foundElement = data[category].find(element => element.id === id);
+      if (foundElement) {
+        return foundElement;
+      }
+    }
+  }
+  return null;
+}
 
 
-const crearStrings = () => {
-    const colores = [
-        {"esp": "rojo", "ing": "red"},
-        {"esp": "azul", "ing": "blue"},
-        {"esp": "verde", "ing": "green"},
-        {"esp": "amarillo", "ing": "yellow"},
-        {"esp": "negro", "ing": "black"},
-        {"esp": "blanco", "ing": "white"},
-        {"esp": "morado", "ing": "purple"},
-        {"esp": "naranja", "ing": "orange"},
-        {"esp": "rosa", "ing": "pink"},
-        {"esp": "gris", "ing": "gray"},
-        {"esp": "marrón", "ing": "brown"},
-        {"esp": "violeta", "ing": "violet"},
-        {"esp": "cian", "ing": "cyan"},
-        {"esp": "magenta", "ing": "magenta"},
-        {"esp": "turquesa", "ing": "turquoise"},
-        {"esp": "lavanda", "ing": "lavender"},
-        {"esp": "beige", "ing": "beige"},
-        {"esp": "crema", "ing": "cream"},
-        {"esp": "dorado", "ing": "golden"},
-        {"esp": "plateado", "ing": "silver"}
-      ];
-      
-    let array_nombres = colores.map((obj) => {
-        let string = `${obj.ing}_${obj.esp}`;
-        return string;
-    });
-    console.log(array_nombres)
-};
-
-
-module.exports = crearStrings;
+module.exports = findElementById;
--- index.js
+++ index.js
@@ -4,78 +4,114 @@ const port = 3000;
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-const crearStrings = require('./funcionalidades.js');
+const findElementById = require('./funcionalidades')
 
 app.use(cors({
     origin: '*'
 }));
 
-crearStrings()
+/* app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://tudominio.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  }); */
+
 app.get('/', (req, res) => {
     res.send('¡Hola, Mundo!');
 });
+/* ------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------ */
+
+let dataCache = null; // Variable para almacenar los datos
+
+const loadData = () => {
+    return new Promise((resolve, reject) => {
+        const directorio = './media/';
+        fs.readdir(directorio, (err, archivos) => {
+            if (err) {
+                console.error("Error en lectura de carpeta: ", err);
+                return reject('Error al leer carpeta');
+            }
+
+            let id = 0;
+            const promesasDeLectura = archivos.map(archivo => {
+                return new Promise((resolve, reject) => {
+                    fs.readdir(path.join(directorio, archivo), (err, archivosImgs) => {
+                        if (err) {
+                            console.error('Error al leer la carpeta:', err);
+                            return reject(err);
+                        }
+                        const arrayUrlsImgs = archivosImgs.map(nombreImagen => {
+                            id++;
+                            let separacionDePartes = nombreImagen.split('_');
+                            let texto_español = separacionDePartes[1]?.split('.')[0];
+                            let texto_ingles = separacionDePartes[0];
+                            return {
+                                id: id,
+                                url: `https://e-b-js-traduciones.onrender.com/media/${archivo}/${nombreImagen}`,
+                                ing: `${texto_ingles.charAt(0).toUpperCase() + texto_ingles.slice(1).replace(/\.[^.]+$/, '')}`,
+                                esp: `${texto_español?.charAt(0).toUpperCase() + texto_español?.slice(1).replace(/\.[^.]+$/, '')}`,
+                                activo: false
+                            };
+                        });
+                        resolve({ [archivo]: arrayUrlsImgs });
+                    });
+                });
+            });
+
+            Promise.all(promesasDeLectura)
+                .then(objetos => {
+                    const objetoReturn = objetos.reduce((resultado, objeto) => {
+                        return { ...resultado, ...objeto };
+                    }, {});
+                    resolve(objetoReturn);
+                })
+                .catch(error => {
+                    console.error('Error general:', error);
+                    reject('Error interno del servidor');
+                });
+        });
+    });
+};
+
+const initializeData = async () => {
+    try {
+        dataCache = await loadData();
+        console.log('Datos cargados con éxito');
+    } catch (error) {
+        console.error('Error al cargar datos:', error);
+    }
+};
+
+initializeData();
+
+app.get('/data-elements', (req, res) => {
+    if (dataCache) {
+        res.json(dataCache);
+    } else {
+        res.status(500).send('Datos no disponibles');
+    }
+});
+
+app.get('/element/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const element = findElementById(dataCache, id);
+
+    if (element) {
+        res.json(element);
+    } else {
+        res.status(404).send('Elemento no encontrado');
+    }
+});
+  
 
 /* RETORNA OBJETO CON TODAS LAS RUTAS DE IMAGENES TRADUCCIONES EN ESPAÑOL E INGLES DE LO QUE SE REPRESENTA */
-app.get('/data-elements', (req, res) => {
-
-    const directorio = './media/'
-
-    fs.readdir(directorio, (err, archivos) => {
-
-        if (err) {
-            console.error("Error en lectura de carpeta: ", err)
-            return res.status(500).send('Error al leer carpeta')
-        }
-
-        let id = 0
-
-        const promesasDeLectura = archivos.map(archivo => {
-            return new Promise((resolve, reject) => {
-
-                fs.readdir(path.join(directorio, archivo), (err, archivosImgs) => {
-                    if (err) {
-                        console.error('Error al leer la carpeta:', err);
-                        return reject(err);
-                    }
-                    const arrayUrlsImgs = archivosImgs.map(nombreImagen => {
-                        id++
-                        let separacionDePartes = nombreImagen.split('_')
-                        let texto_español = separacionDePartes[1]?.split('.')[0]
-                        let texto_ingles = separacionDePartes[0]
-                        return (
-                            {
-                                id: id,
-                                url: `/media/${archivo}/${nombreImagen}`,
-                                /* CONVIERTE PRIMER LETRA EN MAYUSCULAS, Y ELIMINA EXTENSION, TOMANDO EL NOMBRE DE LA IMAGEN COMO NOMBRE PRINCIPAL */
-                                ing:`${texto_ingles.charAt(0).toUpperCase() + texto_ingles.slice(1).replace(/\.[^.]+$/, '')}`,
-                                esp: `${texto_español?.charAt(0).toUpperCase() + texto_español?.slice(1).replace(/\.[^.]+$/, '')}`,
-                                activo:false
-                            }
-                        )
-
-                    })
-
-                    resolve({ [archivo]: arrayUrlsImgs })
-                })
-            })
-        })
-        // Esperar a que todas las promesas de lectura se resuelvan
-        Promise.all(promesasDeLectura)
-            .then(objetos => {
-                // Construir el objetoReturn una vez que todas las promesas se hayan resuelto
-                const objetoReturn = objetos.reduce((resultado, objeto) => {
-                    return { ...resultado, ...objeto }
-                }, {})
-
-                console.log(objetoReturn)
-                res.json(objetoReturn)
-            })
-            .catch(error => {
-                console.error('Error general:', error)
-                res.status(500).send('Error interno del servidor')
-            })
-
-    })
+app.get('/categories/keys', (req, res) => {
+    const keywords = Object.keys(dataCache)
+    console.log(keywords)
+    res.json(keywords);
 })
 
 app.get('/media/:categoria/:imageName', (req, res) => {
@@ -85,7 +121,7 @@ app.get('/media/:categoria/:imageName', (req, res) => {
     res.sendFile(imagePath); // Envía el archivo de imagen al cliente
 })
 
-app.get('/completed', (req, res)=>{
+app.get('/completed', (req, res) => {
     const imagePath = path.join(__dirname, `./completed.jpg`); // RUTA API
     res.sendFile(imagePath)
 })
@@ -93,7 +129,7 @@ app.get('/completed', (req, res)=>{
 app.get('/ping', (req, res) => {
     console.log(`ping`);
     res.status(200).send('pong traducc')
-  });
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
